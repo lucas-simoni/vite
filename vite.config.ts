2c0a1bcf173260ea02c8a9ca226017b5f3dd99a6
lucas-simoni <lucas.simoni@wexinc.com>
2022-11-03 18:00:49 -0300

    feat(test): add vitest and testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   envDir: './config',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    coverage: {
+      provider: 'c8',
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+      all: true,
+      include: ['src/**/*.{ts,tsx}', '!src/index.tsx', '!src/app/index.tsx'],
+    },
+  },
 });
